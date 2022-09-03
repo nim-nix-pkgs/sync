32c16f8360559bf7fd830a1c380aab87cf77deb1
hugosenari <hugosenari@gmail.com>
2022-09-03 19:53:17 -0300

    feat(nimpkgs): update lock

--- source/sync.nim
+++
@@ -1,5 +0,0 @@
-when not compileOption("threads"):
-  {.error: "This module requires --threads:on compilation flag".}
-
-import sync/[atomics2, barrier, latch, once, rwlock, semaphore, smartptrs, spinlock, spsc, spsc_queue]
-export atomics2, barrier, latch, once, rwlock, semaphore, smartptrs, spinlock, spsc, spsc_queue
